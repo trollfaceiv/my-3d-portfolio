d07a58c9d1dc235fb1d9cb95200a86cfdaf287f0
gennaro <g.rascato@gmail.com>
2023-05-19 23:19:32 +0200

    Aggiunta modello 3D in <Hero>

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { style } from '../style'
+import { ComputersCanvas } from './canvas'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`${style.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto
+      flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
+          <div className="w-1 sm:h-80 h-40 violet-gradient"></div>
+        </div>
+        <div>
+          <h1 className={`${style.heroHeadText} text-white`}>
+            Ciao sono <span className="text-[#915eff]">Pincer</span></h1>
+          <p className={`${style.heroSubText} text-white mt-2`}>
+            Combatto con i mantesiCombatto con
+            <br className="sm:block hidden"></br>i mantesiCombatto con i mantesi
+          </p>
+        </div>
+      </div>
+      
+    <ComputersCanvas />
+    </section>
   )
 }
 
