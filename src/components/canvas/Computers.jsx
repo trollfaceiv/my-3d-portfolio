abfab78f7d73ac23a38a3046550e015e62bcb26d
gennaro <g.rascato@gmail.com>
2023-05-20 13:41:30 +0200

    Aggiunto logica di scrolling e transizioni

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = ({isMobile}) => {
   const computer = useGLTF('/desktop_pc/scene.gltf');
   return (
     <mesh>
@@ -15,14 +15,34 @@ const Computers = () => {
         
       <primitive
        object={computer.scene}
-       scale={0.75}
-       position={[0,-3.25,-1.5]} 
+       scale={ isMobile ? 0.55 : 0.75}
+       position={isMobile ? [-3, -3, -2.2] : [0,-3.25,-1.5]} 
        rotation= {[-0.01, -0.2, -0.1]}/>
     </mesh>
   )
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (e) => {
+      setIsMobile(e.matches);
+    }
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change',
+      handleMediaQueryChange);
+    };
+  }, [])
+
+
   return(
     <Canvas
     frameloop="demand"
@@ -35,7 +55,7 @@ const ComputersCanvas = () => {
          enableZoom={false}
          maxPolarAngle={Math.PI / 2}
          minPolarAngle={Math.PI / 2}/>
-         <Computers />
+         <Computers isMobile={isMobile}/>
         </Suspense>
         <Preload all />
     </Canvas>
