e367e8b5019d68f30a537a05c2f3ca1d0c3f36e3
gennaro <g.rascato@gmail.com>
2023-05-21 13:24:53 +0200

    Commit final

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -11,7 +11,22 @@ const FeedbackCard = ({index, testimonial, name, designation, company,image}) =>
   className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'>
     <p className="text-white font-black text-[48px]">"</p>
     <div className='mt-1'>
-    <p>{testimonial}</p>
+    <p className='text-white tracking-wider text-[18px]'>{testimonial}</p>
+    <div className='mt-7 flex justify-between items-center gap-1'>
+      <div className='flex-1 flex flex-col'>
+        <p className='text-white font-medium text-[16px]'>
+          <span className='blue-text-gradient'>@</span> {name}
+        </p>
+        <p className='mt-1 text-secondary text-[12px]'>
+          {designation} of {company}
+        </p>
+      </div>
+      <img
+      src={image}
+      alt={`feedback-by-${name}`}
+      className='w-10 h-10 rounded-full object-cover'>
+      </img>
+    </div>
     </div>
 
   </motion.div>)
